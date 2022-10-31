395030798995d3fc592deb0c4fecd53522586f6c
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-31 09:01:44 -0300

    insert into tb_clientes and tb_vendedores

---
+++ Insert-Into-tables.sql
@@ -0,0 +1,32 @@
+-- Insert into TB_CLIENTES
+INSERT INTO tb_clientes(cpf,nome,cep,data_nacimento,idade,sexo, limite_de_credito, volume_compra , primera_compra) 
+VALUES ('36651819330', 'Maria Renata Assunção', '64010838', '1980-04-27', 42, 'F', 2000.0, 200, 0);
+
+INSERT INTO tb_clientes(cpf,nome,cep,data_nacimento,idade,sexo, limite_de_credito, volume_compra , primera_compra) 
+VALUES ('70734828268', 'Hugo Benedito Victor da Rocha', '67030040', '1967-01-13', 55, 'M', 10000.0, 4000, 0);
+
+INSERT INTO tb_clientes(cpf,nome,cep,data_nacimento,idade,sexo, limite_de_credito, volume_compra , primera_compra) 
+VALUES ('92719267902', 'Bento Benício Paulo Sales', '69023262', '1988-06-27', 34, 'M', 5000.0, 250, 0);
+
+INSERT INTO tb_clientes(cpf,nome,cep,data_nacimento,idade,sexo, limite_de_credito, volume_compra , primera_compra) 
+VALUES ('73455364160', 'Larissa Sônia Heloisa Gomes', '69316266', '1977-08-24', 45, 'F', 6400.0, 440, 0);
+
+INSERT INTO tb_clientes(cpf,nome,cep,data_nacimento,idade,sexo, limite_de_credito, volume_compra , primera_compra) 
+VALUES ('07981967856', 'Leonardo Calebe Roberto Pires', '78120635', '1995-08-25', 27, 'M', 500.0, 10, 0);
+
+
+-- Insert into TB_VENDEDORES
+INSERT INTO tb_vendedores (nome, bairro,comissao,data_admissao,ferias) 
+VALUES ('Alessandra Joana Mirella Freitas', 'Fortaleza' , 2424.0, '2022-01-01', '2023-01-01');
+
+INSERT INTO tb_vendedores (nome, bairro,comissao,data_admissao,ferias) 
+VALUES ('Mariah Sandra Castro', 'Recanto das Emas' , 1212.0, '2022-06-01', '2023-06-01');
+
+INSERT INTO tb_vendedores (nome, bairro,comissao,data_admissao,ferias) 
+VALUES ('Tiago Alexandre Leonardo Pereira', 'Alvorada' , 1212.0, '2022-06-01', '2023-06-01');
+
+INSERT INTO tb_vendedores (nome, bairro,comissao,data_admissao,ferias) 
+VALUES ('Sueli Flávia Marcela Baptista', 'Conjunto Tucumã' , 1212.0, '2022-06-01', '2023-06-01');
+
+INSERT INTO tb_vendedores (nome, bairro,comissao,data_admissao,ferias) 
+VALUES ('Clara Giovana Isabella das Neves', 'Novo Horizonte' , 1212.0, '2022-06-01', '2023-06-01');
